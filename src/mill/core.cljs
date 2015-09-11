8f078ef9b25bdde87c968cae54a340fa9022ddee
Chris LaRose <chris.larose@mycase.com>
2015-09-10 21:11:53 -0700

    cjl - Add basic machine definition and implementation of con instruction

--- src/mill/core.cljs
+++ src/mill/core.cljs
@@ -1,6 +1,23 @@
 (ns mill.core
   (:require [clojure.browser.repl :as repl]))
 
+(def imaginary-member
+  {:belt-size 8})
+
+(defn machine [{:keys [belt-size] :as family-member}]
+  {:belt (repeat belt-size :none)
+   :family-member family-member})
+
+(defn execute-op [{:keys [belt family-member] :as m} op & args]
+  (->> (apply op belt args)
+       (apply conj belt)
+       (take (:belt-size family-member))
+       (assoc m :belt)))
+
+(defn con [belt constant]
+  [constant])
+
+
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
