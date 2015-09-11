353da619d4100b510d8f5369f19a04ee1950fcc9
Chris LaRose <chris.larose@mycase.com>
2015-09-10 22:16:59 -0700

    cjl - addf

--- src/mill/core.cljs
+++ src/mill/core.cljs
@@ -14,9 +14,36 @@
        (take (:belt-size family-member))
        (assoc m :belt)))
 
+(defn none? [operand]
+  (and (not (:valid? operand)) (= (:value operand) 0)))
+
+(defn nar? [operand]
+  (and (not (:valid? operand)) (not= (:value operand) 0)))
+
+(defn w [constant]
+  {:width 4 :scalarity 0 :valid? true :value constant})
+
 (defn con [belt constant]
   [constant])
 
+(defn belt-nth [belt pos-sym]
+  (->> pos-sym
+       (name)
+       (drop 1)
+       (apply str)
+       (js/parseInt)
+       (nth belt)))
+
+(defn addf [belt a b]
+  (let [lhs (belt-nth belt a)
+        rhs (belt-nth belt b)]
+    (cond
+      (or (nar? lhs) (nar? rhs))
+        [{:width 4 :scalarity 0 :valid? false :value -1}]
+      (or (none? lhs) (none? rhs))
+        [{:width 4 :scalarity 0 :valid? false :value 0}]
+      :else
+        [{:width 4 :scalarity 0 :valid? true :value (+ (:value lhs) (:value rhs))}])))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
