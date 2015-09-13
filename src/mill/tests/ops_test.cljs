ddbf2cb5f3cadfb59dfd67d1cad9217f026bdb07
Chris LaRose <chris.larose@mycase.com>
2015-09-13 12:04:12 -0700

    cjl - Add tests for addux(v)

--- src/mill/tests/ops_test.cljs
+++ src/mill/tests/ops_test.cljs
@@ -1,6 +1,6 @@
 (ns mill.tests.ops-test
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
-            [mill.tests.util :refer [is-element]]
+            [mill.tests.util :refer [is-element is-element-nar]]
             [mill.slice :as slice]
             [mill.ops :as ops]))
 
@@ -50,6 +50,20 @@
           (is (= 4 byte-width))
           (is (= 2 (count elements)))
           (is-element {:valid? true :buffer [255 255 255 255]} (first elements))
+          (is-element {:valid? true :buffer [0 0 0 118]} (second elements)))))
+    (testing "excepting"
+      (testing "with scalars"
+        (let [sum    (ops/addu :excepting (slice/int-slice -1) (slice/int-slice 2))
+              {:keys [byte-width elements]} sum]
+          (is (= 4 byte-width))
+          (is (= 1 (count elements)))
+          (is-element-nar (first elements))))
+      (testing "with vectors"
+        (let [sum    (ops/addu :excepting (slice/int-slice 65 97) (slice/int-slice -1 21))
+              {:keys [byte-width elements]} sum]
+          (is (= 4 byte-width))
+          (is (= 2 (count elements)))
+          (is-element-nar (first elements))
           (is-element {:valid? true :buffer [0 0 0 118]} (second elements)))))))
 
 (run-tests)
