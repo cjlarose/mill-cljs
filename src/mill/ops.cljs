6f448ff79352d0ea5ddf7b5200ce1be54d4e5d8b
Chris LaRose <chris.larose@mycase.com>
2015-09-12 21:28:03 -0700

    cjl - Formatting

--- src/mill/ops.cljs
+++ src/mill/ops.cljs
@@ -51,18 +51,18 @@
                  (addu-buffers (:buffer a) (:buffer b)))
         results (map el-add element-pairs)]
     (if (#{:modulo :saturating :excepting} overflow)
-      (let [result-handler (fn [[sum carry]]
-                             (case overflow
-                               :modulo
-                                 {:valid? true
-                                  :buffer (->buffer sum)}
-                               :saturating
-                                 {:valid? true
-                                  :buffer (->buffer (if (= carry 0)
-                                                      sum
-                                                      (repeat (:byte-width x) 255)))}))]
+      (let [f (fn [[sum carry]]
+                (case overflow
+                  :modulo
+                    {:valid? true
+                     :buffer (->buffer sum)}
+                  :saturating
+                    {:valid? true
+                     :buffer (->buffer (if (= carry 0)
+                                         sum
+                                         (repeat (:byte-width x) 255)))}))]
         {:byte-width (:byte-width x)
-         :elements   (map result-handler results)}))))
+         :elements   (map f results)}))))
 
   ; (let [overflowed? (= carry 1)
   ;       to-value    (fn [byte-seq]
