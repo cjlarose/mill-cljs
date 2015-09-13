42aa7f19c2f32b461d60079c1672bcc1d515078b
Chris LaRose <chris.larose@mycase.com>
2015-09-13 00:18:34 -0700

    cjl - Remove obsolute functions

--- src/mill/slice.cljs
+++ src/mill/slice.cljs
@@ -1,14 +1,5 @@
 (ns mill.slice)
 
-; (defn slice
-;   "Makes a new, zeroed-out slice of the given byte-width and length"
-;   ([length byte-width]
-;    (slice length byte-width 0))
-;   ([length byte-width fill-value]
-;    {:byte-width byte-width
-;     :buffer (doto (js/Buffer. (* length byte-width))
-;               (.fill fill-value))}))
-
 (defn scalar?
   "Returns whether the slice is a scalar. Scalars in Mill are just
   vectors of length 1"
@@ -35,19 +26,3 @@
   [{:keys [byte-width elements]}]
   (let [halves (split-at (/ (count elements) 2) elements)]
     (mapv (fn [v] {:byte-width byte-width :elements v}) halves)))
-
-; (defn from-octet-seq [byte-width octet-seq]
-;   {:byte-width byte-width
-;    :buffer (js/Buffer. (clj->js octet-seq))})
-; 
-; (defn octet-seq
-;   "Returns the list of octets of a slice"
-;   [{:keys [buffer]}]
-;   (map #(aget buffer %) (range (.-length buffer))))
-; 
-; (defn- elements
-;   "Returns a seq of buffers representing elements of the given slice"
-;   [{:keys [buffer byte-width]}]
-;   (map
-;     (fn [offset] (.slice buffer offset (+ offset byte-width)))
-;     (range 0 (.-length buffer) byte-width)))
