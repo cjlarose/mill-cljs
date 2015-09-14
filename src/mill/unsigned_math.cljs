f41fb6139644a3df39818438bb9d4f98ebd4ee5c
Chris LaRose <chris.larose@mycase.com>
2015-09-13 17:18:00 -0700

    cjl - addu and addus now operate on MaybeNaRs

--- src/mill/unsigned_math.cljs
+++ src/mill/unsigned_math.cljs
@@ -1,17 +1,25 @@
 (ns mill.unsigned-math
-  (:require [mill.buffer :refer [addu-buffers ->buffer]]))
+  (:require [mill.buffer :refer [addu-buffers ->buffer]]
+            [mill.nar :refer [result]]
+            [mill.ct :refer [fapply fmap]]))
 
 (def deadbeef '(222 173 190 239))
 
-(defn addu [x y]
+(defn- addu-buffer [x y]
   (let [[sum _] (addu-buffers x y)]
     (->buffer sum)))
 
-(defn addus [x y]
+(defn addu [x y]
+  (fapply (fmap x #(partial addu-buffer %)) y))
+
+(defn- addus-buffer [x y]
   (let [[sum carry] (addu-buffers x y)]
     (if (= carry 0)
       (->buffer sum)
       (->buffer (repeat (.-length x) 255)))))
+
+(defn addus [x y]
+  (fapply (fmap x #(partial addus-buffer %)) y))
 
 (defn adduw [x y]
   (let [[sum carry] (addu-buffers x y)]
